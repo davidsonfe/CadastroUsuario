851997fda0c969c1421c80425d7a6c4468fbce99
Davidson Felix <davidsonfeli@gmail.com>
2021-06-26 20:17:09 -0300

    first commit

---
+++ ConnectionFactory.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory;
+
+/**
+ *
+ * @author david
+ */
+
+// faz as importações de classes necessárias para o funcionamento do programa 
+import java.sql.Connection; 
+// conexão SQL para Java 
+import java.sql.DriverManager; 
+// driver de conexão SQL para Java 
+import java.sql.SQLException; 
+// classe para tratamento de exceções 
+
+public class ConnectionFactory {
+    
+     public Connection getConnection() {
+		 try {
+			return DriverManager.getConnection("jdbc:mysql://localhost:8080/projetojava","seu-nome-de-usuario","sua-senha");
+		 }         
+		 catch(SQLException excecao) {
+			throw new RuntimeException(excecao);
+		 }
+    }
+}
